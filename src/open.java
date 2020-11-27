4f7ce1a513eafa7f192502764f4ce65c736fc5d7
Liuzuoteng <liuzuoteng0428@163.com>
2020-11-28 01:59:24 +0800

    test

---
+++ src/open.java
@@ -0,0 +1,11 @@
+/**
+ * description: open
+ * date: 2020/11/28 1:54
+ * author: lzt
+ * version: 1.0
+ */
+public class open {
+    public static void main(String[] args) {
+        System.out.println("操你妈的美国 谷歌");
+    }
+}
